1c984733a81d438e6eb03721e180318bf9fc17cb
Trob1e <petukhin1992@mail.ru>
2021-12-13 19:42:35 +0300

    add openGraphics method in MainFrame class, in GraphicsDisplay showGraphics method

--- src/GraphicsDisplay.java
+++ src/GraphicsDisplay.java
@@ -1,5 +1,79 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class GraphicsDisplay extends JPanel {
+    private ArrayList<Double[]> graphicsData;
+    private ArrayList<Double[]> originalData;
 
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+
+    private Font axisFont;
+    private Font labelsFont;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    private BasicStroke gridStroke;
+    private BasicStroke selectionStroke;
+
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+
+    public GraphicsDisplay() {
+        this.setBackground(Color.WHITE);
+        this.axisStroke = new BasicStroke(2.0F, 0, 1, 10.0F, null, 0.0F);
+        this.markerStroke = new BasicStroke(1.5F, 0, 1, 5.0F, new float[] {30, 30, 10, 10, 10, 10, 20, 20, 20, 20}, 0.0F);
+        this.selectionStroke = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{10.0F, 10.0F}, 0.0F);
+
+
+        this.gridStroke = new BasicStroke(0.5F, 0, 1, 5.0F, new float[]{5.0F, 5.0F}, 2.0F);
+        this.axisFont = new Font("Serif", 1, 36);
+        this.labelsFont = new Font("Serif", 0, 10);
+    }
+
+    public void showGraphics(ArrayList<Double[]> graphicsData) {
+        this.graphicsData = graphicsData;
+        this.originalData = new ArrayList<>(graphicsData.size());
+        Iterator var3 = graphicsData.iterator();
+
+        while(var3.hasNext()){
+            Double[] point = (Double[])var3.next();
+            Double[] newPoint = new Double[]{point[0], point[1]};
+            this.originalData.add(newPoint);
+        }
+
+        this.minX = (graphicsData.get(0))[0];
+        this.maxX = (graphicsData.get(graphicsData.size() - 1))[0];
+        this.minY = (graphicsData.get(0))[1];
+        this.maxY = this.minY;
+
+        for(int i = 1; i < graphicsData.size(); ++i) {
+            if ((graphicsData.get(i))[1] < this.minY) {
+                this.minY = (graphicsData.get(i))[1];
+            }
+
+            if ((graphicsData.get(i))[1] > this.maxY) {
+                this.maxY = (graphicsData.get(i))[1];
+            }
+        }
+
+
+    }
+
+    public void reset() {
+        this.showGraphics(this.originalData);
+    }
+
+    public void setShowAxis(boolean showAxis) {
+        this.showAxis = showAxis;
+        this.repaint();
+    }
+
+    public void setShowMarkers(boolean showMarkers) {
+        this.showMarkers = showMarkers;
+        this.repaint();
+    }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,120 @@
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.util.ArrayList;
 
 public class MainFrame extends JFrame {
+    private boolean fileLoaded = false;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private JMenuItem resetGraphicsMenuItem;
+    private JFileChooser fileChooser = null;
+
+    public MainFrame() {
+        super("Вывод графика функции");
+        this.setSize(600, 600);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        this.setLocation((kit.getScreenSize().width - 600) / 2, (kit.getScreenSize().height - 600) / 2);
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+
+        Action openGraphicsAction = new AbstractAction("Открыть файл") {
+            public void actionPerformed(ActionEvent arg0) {
+                if (MainFrame.this.fileChooser == null) {
+                    MainFrame.this.fileChooser = new JFileChooser();
+                    MainFrame.this.fileChooser.setCurrentDirectory(new File("."));
+                }
+
+                if (MainFrame.this.fileChooser.showOpenDialog(MainFrame.this) == 0) {
+                }
+
+                MainFrame.this.openGraphics(MainFrame.this.fileChooser.getSelectedFile());
+            }
+        };
+        fileMenu.add(openGraphicsAction);
+
+
+
+        JMenu graphicsMenu = new JMenu("График");
+        menuBar.add(graphicsMenu);
+
+        Action showAxisAction = new AbstractAction("Показать оси координат") {
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.display.setShowAxis(MainFrame.this.showAxisMenuItem.isSelected());
+            }
+        };
+        this.showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        graphicsMenu.add(this.showAxisMenuItem);
+        this.showAxisMenuItem.setSelected(true);
+        Action showMarkersAction = new AbstractAction("Показать маркеры точек") {
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.this.display.setShowMarkers(MainFrame.this.showMarkersMenuItem.isSelected());
+            }
+        };
+        this.showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
+        graphicsMenu.add(this.showMarkersMenuItem);
+        this.showMarkersMenuItem.setSelected(true);
+        graphicsMenu.addSeparator();
+        Action resetGraphicsAction = new AbstractAction("Отменить все изменения") {
+            public void actionPerformed(ActionEvent event) {
+                MainFrame.this.display.reset();
+            }
+        };
+        this.resetGraphicsMenuItem = new JMenuItem(resetGraphicsAction);
+        graphicsMenu.add(this.resetGraphicsMenuItem);
+        this.resetGraphicsMenuItem.setEnabled(false);
+        graphicsMenu.addMenuListener(new MainFrame.GraphicsMenuListener());
+        this.getContentPane().add(this.display, "Center");
+    }
+
+    private class GraphicsMenuListener implements MenuListener {
+        private GraphicsMenuListener() {
+        }
+
+        public void menuCanceled(MenuEvent arg0) {
+        }
+
+        public void menuDeselected(MenuEvent arg0) {
+        }
+
+        public void menuSelected(MenuEvent arg0) {
+            MainFrame.this.showAxisMenuItem.setEnabled(MainFrame.this.fileLoaded);
+            MainFrame.this.showMarkersMenuItem.setEnabled(MainFrame.this.fileLoaded);
+
+        }
+    }
+
+    protected void openGraphics(File selectedFile) {
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+            ArrayList graphicsData = new ArrayList(50);
+
+            while(in.available() > 0) {
+                Double x = in.readDouble();
+                Double y = in.readDouble();
+                graphicsData.add(new Double[]{x, y});
+            }
+
+            if (graphicsData.size() > 0) {
+                this.fileLoaded = true;
+                this.resetGraphicsMenuItem.setEnabled(true);
+                this.display.showGraphics(graphicsData);
+            }
+        } catch (FileNotFoundException var6) {
+        } catch (IOException var7) {
+        }
+    }
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
 
 }
